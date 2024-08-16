0068deca6074890cd9845f22b39f658c1930de83
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2024-08-16 12:25:05 +0200

    run pre-commit

--- access-esgf.py
+++ access-esgf.py
@@ -1,21 +1,35 @@
 from cordex import esgf_access
 
-import pandas as pd
 
 # Euro-Cordex search attributes
-attrs = {'project':'CORDEX', 'domain':'EUR-11'}
+attrs = {"project": "CORDEX", "domain": "EUR-11"}
 
 # create a datasets search result object
 results = esgf_access.datasets(attrs, verbose=True)
 
 # creates a pandas dataframe from dataset ids
-df = esgf_access.to_pandas(results, columns='CORDEX')
+df = esgf_access.to_pandas(results, columns="CORDEX")
 print(df)
-df.to_csv('euro-cordex-esgf.csv', index=False)
+df.to_csv("euro-cordex-esgf.csv", index=False)
 
 # regroup dataframe for excel output
-cordex_list = df.groupby(['institute', 'model_id', 'driving_model_id', 'experiment_id', 
-                          'member', 'frequency', 'date', 'host', 'domain'])['variable'].apply(list).to_frame()
+cordex_list = (
+    df.groupby(
+        [
+            "institute",
+            "model_id",
+            "driving_model_id",
+            "experiment_id",
+            "member",
+            "frequency",
+            "date",
+            "host",
+            "domain",
+        ]
+    )["variable"]
+    .apply(list)
+    .to_frame()
+)
 print(cordex_list)
 
-cordex_list.to_excel('euro-cordex-esgf.xlsx')
+cordex_list.to_excel("euro-cordex-esgf.xlsx")
